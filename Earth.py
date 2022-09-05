1a68026fdeff032b6a34bb708b3a6a9ce7a5bac6
Jotham <jothamsamuel05@gmail.com>
2022-09-05 18:57:51 +0530

    ADD_EARTH.PY_AND_EARTH.EXE

---
+++ Earth.py
@@ -0,0 +1,61 @@
+#import libraries
+
+import time
+import os
+
+#Intro
+
+print("Get ready to view the Earth on your console...")
+time.sleep(1)
+
+#Earth drawing
+
+print("""
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[46m                                     \u001b[0m
+\u001b[42m    \u001b[46m  \u001b[42m                               \u001b[0m
+\u001b[42m    \u001b[46m  \u001b[42m                               \u001b[0m
+\u001b[42m    \u001b[46m  \u001b[42m                               \u001b[0m
+\u001b[46m                                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[46m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+""")
+
+
+# do not close stuff
+
+while 1:
+    input("Enter to rerun")
+    os.system("cls")
+    
+    print("Get ready to view the Earth on your console...")
+    time.sleep(1)
+
+    print("""
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[46m                                     \u001b[0m
+\u001b[42m    \u001b[46m  \u001b[42m                               \u001b[0m
+\u001b[42m    \u001b[46m  \u001b[42m                               \u001b[0m
+\u001b[42m    \u001b[46m  \u001b[42m                               \u001b[0m
+\u001b[46m                                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[42m             \u001b[46m   \u001b[42m                     \u001b[0m
+\u001b[46m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+\u001b[47m                                     \u001b[0m
+""")
